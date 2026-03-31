1ef1326680a059d908c3c214fd51415821100454
Pratham-Dusane <dusane.pratham@gmail.com>
2026-03-31 14:26:30 +0530

    deploy

--- stitch/vite.config.js
+++ stitch/vite.config.js
@@ -26,7 +26,12 @@ export default defineConfig({
         master_parser: 'master_parser_sleek/code.html',
         remittance_835: '835_remittance_sleek/code.html',
         enrollment_834: '834_enrollment_sleek/code.html',
-        claims_837: '837_claims_view/code.html'
+        claims_837: '837_claims_view/code.html',
+        notifications: 'notifications/code.html',
+        settings: 'settings/code.html',
+        user_profile: 'user_profile/code.html',
+        documentation: 'documentation/code.html',
+        help_center: 'help_center/code.html'
       }
     }
   }
